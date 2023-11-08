45282a3ca930c07e5526792c50b56d5256fcad25
Martins Rucevskis <martins.rucevskis@gmail.com>
2023-11-08 12:17:47 +0200

    Changed rector config

--- rector.php
+++ rector.php
@@ -4,21 +4,41 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+use Rector\Instanceof_\Rector\Ternary\FlipNegatedTernaryInstanceofRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddMethodCallBasedStrictParamTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
+use Rector\TypeDeclaration\Rector\Property\AddPropertyTypeDeclarationRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/public',
-        __DIR__.'/resources',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
     ]);
 
     // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rules([
+        InlineConstructorDefaultToPropertyRector::class,
+        AddMethodCallBasedStrictParamTypeRector::class,
+        AddPropertyTypeDeclarationRector::class,
+        AddReturnTypeDeclarationBasedOnParentClassMethodRector::class,
+        AddReturnTypeDeclarationRector::class,
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        DeclareStrictTypesRector::class,
+        ReturnTypeFromReturnNewRector::class,
+        ChangeAndIfToEarlyReturnRector::class,
+        FlipNegatedTernaryInstanceofRector::class
+    ]);
 
     // define sets of rules
     $rectorConfig->sets([
