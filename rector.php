7df5ca4142122bae0dc6120c6b9e9e6c8765f91c
Martins Rucevskis <martins.rucevskis@gmail.com>
2023-11-08 11:52:56 +0200

    Changed rector and phpstan config

--- rector.php
+++ rector.php
@@ -21,7 +21,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_81
-    //    ]);
+        $rectorConfig->sets([
+            LevelSetList::UP_TO_PHP_81
+        ]);
 };
